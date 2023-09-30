629d72a564d8f7c906bd2df1452f9d825a3a8145
hound672 <hound672@gmail.com>
2023-09-30 12:19:04 +0300

    [HW 02] Unpack string

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,54 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"strconv"
+	"strings"
+	"unicode"
 )
+
+const symbolToEscape string = "\\"
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func Unpack(source string) (string, error) {
+	var symbolToRepeat string
+	var result strings.Builder
+	var isNextSymbolEscaped bool
+
+	for _, symbolRune := range source {
+		currentSymbol := string(symbolRune)
+		switch {
+		case isNextSymbolEscaped:
+			if !(unicode.IsDigit(symbolRune) || currentSymbol == symbolToEscape) {
+				return "", ErrInvalidString
+			}
+			symbolToRepeat = currentSymbol
+			isNextSymbolEscaped = false
+
+		case currentSymbol == symbolToEscape:
+			result.WriteString(symbolToRepeat)
+			symbolToRepeat = ""
+			isNextSymbolEscaped = true
+
+		case unicode.IsDigit(symbolRune):
+			if symbolToRepeat == "" {
+				return "", ErrInvalidString
+			}
+			repeatCount, err := strconv.Atoi(currentSymbol)
+			if err != nil {
+				return "", err
+			}
+			result.WriteString(strings.Repeat(symbolToRepeat, repeatCount))
+			symbolToRepeat = ""
+
+		default:
+			result.WriteString(symbolToRepeat)
+			symbolToRepeat = currentSymbol
+		}
+	}
+	if isNextSymbolEscaped {
+		return "", ErrInvalidString
+	}
+	result.WriteString(symbolToRepeat)
+	return result.String(), nil
 }
