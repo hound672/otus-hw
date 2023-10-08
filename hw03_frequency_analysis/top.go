0b760069d83cdbb8ae1e0ff6964efa389bb7c01c
Vasilii Bliznetsov <hound672@gmail.com>
2023-10-08 20:07:15 +0300

    1

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,13 @@
 package hw03frequencyanalysis
 
+import "strings"
+
 func Top10(_ string) []string {
+
+	source := "cat and dog, one dog,two cats and one man"
+	res := strings.Fields(source)
+	_ = res
+
 	// Place your code here.
 	return nil
 }
