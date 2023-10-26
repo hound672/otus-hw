41b0a7b87eaf6d284a4d407ebb790a52282c3b68
hound672 <hound672@gmail.com>
2023-10-26 13:55:09 +0300

    Add the first try

--- hw05_parallel_execution/run.go
+++ hw05_parallel_execution/run.go
@@ -1,7 +1,9 @@
 package hw05parallelexecution
 
 import (
+	"context"
 	"errors"
+	"sync"
 )
 
 var ErrErrorsLimitExceeded = errors.New("errors limit exceeded")
@@ -10,6 +12,65 @@ type Task func() error
 
 // Run starts tasks in n goroutines and stops its work when receiving m errors from tasks.
 func Run(tasks []Task, n, m int) error {
-	// Place your code here.
+	ctx, cancel := context.WithCancel(context.Background())
+	var wg sync.WaitGroup
+	workerTasks := make(chan Task, len(tasks))
+	errorTasks := make(chan error, len(tasks))
+	var errorsCount int
+
+	// spawn workers
+	wg.Add(n)
+	for i := 0; i < n; i++ {
+		go worker(ctx, &wg, workerTasks, errorTasks)
+	}
+
+	for _, task := range tasks {
+		workerTasks <- task
+	}
+
+	for _ = range errorTasks {
+		errorsCount++
+		if errorsCount >= m {
+			break
+		}
+	}
+	cancel()
+
+	close(workerTasks)
+	wg.Wait()
+	close(errorTasks) // close this channel only after working (to prevent writing close channel panic)
+
 	return nil
+}
+
+func worker(ctx context.Context, wg *sync.WaitGroup, workerTasks <-chan Task, errorsTasks chan<- error) {
+	defer wg.Done()
+
+	for task := range workerTasks {
+		if ctx.Err() != nil {
+			return
+		}
+
+		err := task()
+		if err != nil {
+			errorsTasks <- err
+		}
+	}
+
+	// the second solution
+	// but the first one i like more since there is less the nesting
+	//for {
+	//	select {
+	//	case <-ctx.Done():
+	//		// cancel working
+	//		return
+	//	case task, ok := <-workerTasks:
+	//		if !ok {
+	//			// channel is closed
+	//			return
+	//		}
+	//
+	//		_ = task()
+	//	}
+	//}
 }
