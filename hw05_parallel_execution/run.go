dc1c897a26536d536b185132b8d84374baead19d
hound672 <hound672@gmail.com>
2023-10-30 14:54:36 +0300

    Implement

--- hw05_parallel_execution/run.go
+++ hw05_parallel_execution/run.go
@@ -1,94 +1,44 @@
 package hw05parallelexecution
 
 import (
-	"context"
 	"errors"
 	"sync"
+	"sync/atomic"
 )
 
 var ErrErrorsLimitExceeded = errors.New("errors limit exceeded")
 
 type Task func() error
 
-// Run starts tasks in n goroutines and stops its work when receiving m errors from tasks.
-func Run(tasks []Task, n, m int) error {
-	ctx, cancel := context.WithCancel(context.Background())
-	var wg sync.WaitGroup
-	workerTasks := make(chan Task, len(tasks))
-	errorTasks := make(chan struct{}, len(tasks))
+// Run starts tasks in N goroutines and stops its work when receiving M errors from tasks.
+func Run(tasks []Task, n int, m int) error {
+	workerTasks := make(chan Task)
+	var errorsCount int32
 
-	// spawn workers
+	wg := sync.WaitGroup{}
 	wg.Add(n)
 	for i := 0; i < n; i++ {
-		go worker(ctx, &wg, workerTasks, errorTasks)
+		go func() {
+			defer wg.Done()
+			for task := range workerTasks {
+				err := task()
+				if err != nil {
+					atomic.AddInt32(&errorsCount, 1)
+				}
+			}
+		}()
 	}
-
 	for _, task := range tasks {
+		if atomic.LoadInt32(&errorsCount) >= int32(m) {
+			break
+		}
 		workerTasks <- task
 	}
-	// close channel immediately since won't send tasks to it anymore
+
 	close(workerTasks)
-
-	//hasError := false
-	//var errorsCount int
-
-	// handle errors
-	//wg.Add(1)
-	//go func() {
-	//	defer wg.Done()
-	//
-	//	select {
-	//	case <-ctx.Done():
-	//		return
-	//	case _ = <-errorTasks:
-	//		errorsCount++
-	//		if errorsCount >= m {
-	//			hasError = true
-	//			cancel()
-	//			return
-	//		}
-	//	}
-	//}()
-
 	wg.Wait()
-	cancel()
-	close(errorTasks) // close this channel only after working (to prevent writing close channel panic)
-
-	//if hasError {
-	//	return ErrErrorsLimitExceeded
-	//}
-
-	return nil
-}
-
-func worker(ctx context.Context, wg *sync.WaitGroup, workerTasks <-chan Task, errorsTasks chan<- struct{}) {
-	defer wg.Done()
-
-	for task := range workerTasks {
-		if ctx.Err() != nil {
-			return
-		}
-
-		err := task()
-		if err != nil {
-			errorsTasks <- struct{}{}
-		}
+	if errorsCount >= int32(m) {
+		return ErrErrorsLimitExceeded
 	}
-
-	// the second solution
-	// but the first one i like more since there is less the nesting
-	//for {
-	//	select {
-	//	case <-ctx.Done():
-	//		// cancel working
-	//		return
-	//	case task, ok := <-workerTasks:
-	//		if !ok {
-	//			// channel is closed
-	//			return
-	//		}
-	//
-	//		_ = task()
-	//	}
-	//}
+	return nil
 }
