b6da6ba85dfbd056dc2f81c724a44d2c90c51123
hound672 <hound672@gmail.com>
2023-10-30 17:52:06 +0300

    Add new unittest

--- hw05_parallel_execution/run_test.go
+++ hw05_parallel_execution/run_test.go
@@ -75,10 +75,10 @@ func TestRun(t *testing.T) {
 		var runTasksCount int32
 
 		for i := 0; i < tasksCount; i++ {
-			err := fmt.Errorf("error from task %d", i)
+			i := i
 
+			err := fmt.Errorf("error from task %d", i)
 			tasks = append(tasks, func() error {
-				time.Sleep(time.Millisecond * time.Duration(rand.Intn(100)))
 				atomic.AddInt32(&runTasksCount, 1)
 				return err
 			})
