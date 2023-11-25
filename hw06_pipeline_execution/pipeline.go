3cd1eb493832558b6d19f5f8e21ed9590bc06d4d
Vasilii Bliznetsov <hound672@gmail.com>
2023-11-25 18:08:10 +0300

    remove .sync and renamse project (#6)
    * remove .sync and renamse project
    
    * resolve
    
    * linter
    
    * linter
    
    * fixes
    
    * fixes?
    
    * fixes?
    
    * fixes?
    
    * revert unittest
    
    * new test
    
    * add log
    
    * add log
    
    * 1
    
    * fix?

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -8,7 +8,55 @@ type (
 
 type Stage func(in In) (out Out)
 
-func ExecutePipeline(in In, done In, stages ...Stage) Out {
-	// Place your code here.
-	return nil
+func insertDone(in In, done Bi) Out {
+	out := make(chan interface{})
+	go func() {
+		defer close(out)
+
+		for {
+			select {
+			case <-done:
+				return
+			case vv, ok := <-in:
+				if !ok {
+					return
+				}
+				select {
+				case <-done:
+					return
+				case out <- vv:
+				}
+			}
+		}
+	}()
+	return out
+}
+
+func ExecutePipeline(in In, done Bi, stages ...Stage) Out {
+	if len(stages) == 0 {
+		nilCh := make(chan interface{})
+		close(nilCh)
+		return nilCh
+	}
+
+	out := stages[0](insertDone(in, done))
+
+	for _, stage := range stages[1:] {
+		stage := stage
+
+		out = stage(insertDone(out, done))
+	}
+
+	stageResults := make([]interface{}, 0)
+	for vv := range out {
+		stageResults = append(stageResults, vv)
+	}
+	resChannel := make(chan interface{}, len(stageResults))
+
+	for _, vv := range stageResults {
+		resChannel <- vv
+	}
+	close(resChannel)
+
+	return resChannel
 }
