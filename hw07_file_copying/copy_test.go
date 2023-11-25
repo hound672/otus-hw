11fd62680ac9e06a2da7e43b63344ec78691e90a
Vasilii Bliznetsov <hound672@gmail.com>
2023-11-25 12:00:59 +0300

    implement?

--- hw07_file_copying/copy_test.go
+++ hw07_file_copying/copy_test.go
@@ -1,7 +1,83 @@
 package main
 
-import "testing"
+import (
+	"fmt"
+	"log"
+	"os"
+	"reflect"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
 
 func TestCopy(t *testing.T) {
-	// Place your code here.
+	t.Run("File.size less than offset", func(t *testing.T) {
+		content := []byte("temporary file's content")
+		tmpfile, err := os.CreateTemp("", "test.")
+		if err != nil {
+			require.NoError(t, err)
+		}
+		defer os.Remove(tmpfile.Name())
+
+		if _, err := tmpfile.Write(content); err != nil {
+			require.NoError(t, err)
+		}
+
+		err = Copy(tmpfile.Name(), "/tmp/", 10000, 100)
+		if err != nil {
+			log.Println(err)
+		}
+		require.EqualError(t, err, "offset exceeds file size")
+	})
+
+	t.Run("The infinite file unsupported", func(t *testing.T) {
+		err := Copy("dev/urandom", "testdata/expected.txt", int64(0), int64(0))
+		if err != nil {
+			log.Println(err)
+		}
+		require.EqualError(t, err, "unsupported file: open dev/urandom: no such file or directory")
+	})
+
+	t.Run("Filesize less than limit", func(t *testing.T) {
+		err := Copy("/dev/null", "testdata/expected.txt", int64(0), int64(0))
+		if err != nil {
+			log.Println(err)
+		}
+		require.NoError(t, err)
+	})
+
+	t.Run("Success copy", func(t *testing.T) {
+		dir, err := os.Getwd()
+		if err != nil {
+			fmt.Println(err)
+			require.NoError(t, err)
+		}
+		fmt.Println("dir: ", dir)
+		content := []byte("Hello world")
+		tmpfile, err := os.CreateTemp("", "test.")
+		if err != nil {
+			log.Println(err)
+			require.NoError(t, err)
+		}
+		defer os.Remove(tmpfile.Name())
+		if _, err := tmpfile.Write(content); err != nil {
+			fmt.Println("write file err: ", err)
+			log.Println(err)
+			require.NoError(t, err)
+		}
+		fmt.Println("tmpfile: ", tmpfile.Name())
+		err = Copy(tmpfile.Name(), "testdata/expected.txt", 0, 0)
+		if err != nil {
+			log.Println(err)
+		}
+		file, err := os.ReadFile("testdata/expected.txt")
+		if err != nil {
+			fmt.Println("read file err: ", err)
+			require.NoError(t, err)
+		}
+		actual := string(file)
+		expected := "Hello world"
+		result := reflect.DeepEqual(expected, actual)
+		require.True(t, result)
+	})
 }
