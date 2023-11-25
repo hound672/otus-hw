4d3d4834f2cad93d00afb710c832f7e56b40ed0c
Vasilii Bliznetsov <hound672@gmail.com>
2023-11-25 18:08:10 +0300

    [HW 07] File copying (#7)

--- hw06_pipeline_execution/pipeline_test.go
+++ hw06_pipeline_execution/pipeline_test.go
@@ -91,3 +91,83 @@ func TestPipeline(t *testing.T) {
 		require.Less(t, int64(elapsed), int64(abortDur)+int64(fault))
 	})
 }
+
+func TestPipelineNewTests(t *testing.T) {
+	// Stage generator
+	g := func(_ string, f func(v interface{}) interface{}) Stage {
+		return func(in In) Out {
+			out := make(Bi)
+			go func() {
+				defer close(out)
+				for v := range in {
+					time.Sleep(sleepPerStage)
+					out <- f(v)
+				}
+			}()
+			return out
+		}
+	}
+
+	stages := []Stage{
+		g("Dummy", func(v interface{}) interface{} { return v }),
+		g("Multiplier (* 2)", func(v interface{}) interface{} { return v.(int) * 2 }),
+		g("Adder (+ 100)", func(v interface{}) interface{} { return v.(int) + 100 }),
+		g("Stringifier", func(v interface{}) interface{} { return strconv.Itoa(v.(int)) }),
+	}
+
+	t.Run("empty stages", func(t *testing.T) {
+		in := make(Bi)
+		done := make(Bi)
+
+		for range ExecutePipeline(in, done) {
+		}
+	})
+
+	t.Run("empty channel", func(t *testing.T) {
+		in := make(Bi)
+		data := []int{}
+
+		go func() {
+			for _, v := range data {
+				in <- v
+			}
+			close(in)
+		}()
+
+		result := make([]string, 0, 10)
+		start := time.Now()
+		for s := range ExecutePipeline(in, nil, stages...) {
+			result = append(result, s.(string))
+		}
+		elapsed := time.Since(start)
+
+		require.Equal(t, []string{}, result)
+		require.Less(t, int64(elapsed),
+			int64(sleepPerStage)*int64(len(stages)+len(data)-1)+int64(fault))
+	})
+
+	t.Run("done before read all from pipeline", func(t *testing.T) {
+		in := make(Bi)
+		done := make(Bi)
+		data := []int{1, 2, 3, 4, 5}
+
+		close(done)
+
+		go func() {
+			for _, v := range data {
+				in <- v
+			}
+			close(in)
+		}()
+
+		result := make([]string, 0, 10)
+		start := time.Now()
+		for s := range ExecutePipeline(in, done, stages...) {
+			result = append(result, s.(string))
+		}
+		elapsed := time.Since(start)
+
+		require.Len(t, result, 0)
+		require.Less(t, int64(elapsed), time.Second)
+	})
+}
--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -1,7 +1,14 @@
 package main
 
 import (
+	"bufio"
 	"errors"
+	"fmt"
+	"io"
+	"log"
+	"os"
+
+	"github.com/cheggaaa/pb/v3"
 )
 
 var (
@@ -10,6 +17,41 @@ var (
 )
 
 func Copy(fromPath, toPath string, offset, limit int64) error {
-	// Place your code here.
+	file, err := os.OpenFile(fromPath, os.O_RDWR, 0666) //nolint
+	if err != nil {
+		return fmt.Errorf("%w: %v", ErrUnsupportedFile, err)
+	}
+	defer func() { _ = file.Close() }()
+
+	inf, err := file.Stat()
+	if err != nil {
+		return fmt.Errorf("getting stat: %w", err)
+	}
+
+	buf := bufio.NewReaderSize(file, int(inf.Size()))
+	if offset > inf.Size() {
+		return errors.New("offset exceeds file size")
+	}
+	_, err = file.Seek(offset, io.SeekStart)
+	if err != nil {
+		return fmt.Errorf("%w: %v", ErrOffsetExceedsFileSize, err)
+	}
+	newFile, err := os.Create(toPath)
+	if err != nil {
+		return fmt.Errorf("failed to trying create file: %w", err)
+	}
+	defer func() { _ = newFile.Close() }()
+
+	if limit == 0 || limit > int64(buf.Size()) {
+		limit = int64(buf.Size())
+	}
+
+	bar := pb.Full.Start64(limit)
+	barReader := bar.NewProxyReader(buf)
+	_, err = io.CopyN(newFile, barReader, limit)
+	bar.Finish()
+	if err != nil {
+		log.Println(err)
+	}
 	return nil
 }
