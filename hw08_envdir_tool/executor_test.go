8d1e60a767e4124085c896dd8d3a75f60e67afda
Vasilii Bliznetsov <hound672@gmail.com>
2023-12-10 12:11:19 +0300

    [HW 08] Envdir tool (#8)

--- hw08_envdir_tool/executor_test.go
+++ hw08_envdir_tool/executor_test.go
@@ -1,7 +1,31 @@
 package main
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
 
 func TestRunCmd(t *testing.T) {
-	// Place your code here
+	t.Run("Success", func(t *testing.T) {
+		env := map[string]string{
+			"STR1": "foo",
+			"STR2": "bar",
+		}
+		cmd := []string{"bash", "-c", "echo $STR1$STR2"}
+		code := RunCmd(cmd, env)
+
+		fmt.Println(cmd)
+		require.Equal(t, 0, code)
+	})
+
+	t.Run("Invalid command error", func(t *testing.T) {
+		env := map[string]string{}
+		cmd := []string{"bash", "-c", "ls /xxx"}
+		code := RunCmd(cmd, env)
+
+		fmt.Println(cmd)
+		require.Equal(t, 1, code)
+	})
 }
