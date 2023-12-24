508cfbe6573bc7aa9416e40c79680a8ab32216a0
Vasilii Bliznetsov <hound672@gmail.com>
2023-12-24 17:17:44 +0300

    Fixes

--- hw11_telnet_client/test.sh
+++ hw11_telnet_client/test.sh
@@ -3,7 +3,8 @@ set -xeuo pipefail
 
 go build -o go-telnet
 
-(echo -e "Hello\nFrom\nNC\n" && cat 2>/dev/null) | nc -l localhost 4242 >/tmp/nc.out &
+#(echo -e "Hello\nFrom\nNC\n" && cat 2>/dev/null) | nc -l localhost 4242 >/tmp/nc.out &
+(echo -e "Hello\nFrom\nNC\n" && cat 2>/dev/null) | nc -lp 4242 >/tmp/nc.out &
 NC_PID=$!
 
 sleep 1
